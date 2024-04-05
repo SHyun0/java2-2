2dd48f979c8d259b34e4a2e1f2a6bd6be1df489d
SHyun0 <hyunii0423@gmail.com>
2024-04-05 16:08:26 +0900

    5주차 강의내용

---
+++ src/EX4_5.java
@@ -0,0 +1,25 @@
+public class EX4_5 {
+    String title;
+    String author;
+    void show() { System.out.println (title + " " + author); }
+
+    public EX4_5() {
+        this ("","");
+        System.out.println("생성자 호출됨");
+    }
+
+    public EX4_5(String title) {
+        this(title, "작자미상");
+    }
+
+    public EX4_5(String title, String author) {
+        this.title = title ; this.author = author;
+    }
+
+    public static void main(String [] args) {
+        EX4_5 littlePrince = new EX4_5("어린왕자 , 생텍쥐페리");
+        EX4_5 loveStory = new EX4_5("춘향전");
+        EX4_5 emptyBook = new EX4_5();
+        loveStory.show();
+    }
+}
