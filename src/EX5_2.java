eeaf6178a2bb2edd1990c7861d725edac684a781
SHyun0 <hyunii0423@gmail.com>
2024-04-12 16:53:44 +0900

    6주차 강의

--- src/EX5_2.java
+++ src/EX5_2.java
@@ -1,33 +1,28 @@
-class Point {
+class Point1 {
     private int x, y;
-
-    public Point() {
-        this.x = x;
+    public Point1(int x, int y) {
+        this.x = x; 
         this.y = y;
+}
+    public void showPoint() { 
+        System.out.println("(" + x + "," + y +")");
     }
-
-    public Point(intx, int y){
-        System.out.println("("+ x +","+ y +")");
-    } 
 }
 
-    class ColorPoint extends Point {
-        private String color;
-
-        public ColorPoint(int x, int y, String color) {
-            super(x, y);
-            this.color = color;
-        }
-
-        public void showColorPoint() {
-            System.out.print(color);
-            showPoint();
-        }
+class ColorPoint1 extends Point1 {
+    private String color;
+    public ColorPoint1(int x, int y, String color) {
+        super(x, y);
+        this.color = color;
     }
-
-public class EX5_2{
-    public static void main(String[] args){
-        ColorPoint cp = new ColorPoint(5,6, "blue");
+    public void showColorPoint() { 
+        System.out.print(color);
+        showPoint();
+    }  
+}
+public class EX5_2 {
+    public static void main(String[] args) {
+        ColorPoint1 cp = new ColorPoint1(5, 6, "blue");
         cp.showColorPoint();
     }
 }
