0f44448465acba9033b3401165026b7d614f3aae
SHyun0 <hyunii0423@gmail.com>
2024-05-03 16:30:32 +0900

    9주차 강의

---
+++ src/EX8_1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class EX8_1 extends JFrame{
+    public EX8_1() {
+        setTitle("300x300 스윙프레임만들기");
+        setSize(300,300); 
+        setVisible(true); 
+    }
+public static void main(String[] args) {
+EX8_1 frame = new EX8_1();
+    }
+}
